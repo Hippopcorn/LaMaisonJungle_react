29650acfa9d3abc9726b8a68a19563f0913e27ae
Hippopcorn <elsa.varache@gmail.com>
2024-11-10 22:12:47 +0100

    add

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,14 @@
+import '../styles/Banner.css'
+import logo from '../assets/logo.png'
+
 function Banner() {
-    const titre = "la maison jungle"
-    return (<h1>{titre.toUpperCase()}</h1>)
+    const title = "la maison jungle"
+    return (
+        <div className='lmj-banner'>
+            <img src={logo} alt='logo' className='lmg-logo'></img>
+            <h1>{title.toUpperCase()}</h1>
+        </div>
+    )
 }
 
 export default Banner
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,17 @@
+import '../styles/Cart.css'
+
 function Cart() {
     const priceMonstera = 8
     const priceLierre = 10
     const priceFlower = 15
-    return  <div>
-    <h2>Panier</h2>
-    <ul>
-        <li>{"Monstera : "+ priceMonstera +" €"}</li>
-        <li>{"Lierre : "+ priceLierre +" €"}</li>
-        <li>{"Bouquet de Fleurs :"+ priceFlower +" €"}</li>
-    </ul> 
-    <p>Total du panier : {priceMonstera + priceLierre + priceFlower +" €"}</p>
+    return <div className='lmj-cart'>
+        <h2>Panier</h2>
+        <ul>
+            <li>{"Monstera : " + priceMonstera + " €"}</li>
+            <li>{"Lierre : " + priceLierre + " €"}</li>
+            <li>{"Bouquet de Fleurs :" + priceFlower + " €"}</li>
+        </ul>
+        <p>Total du panier : {priceMonstera + priceLierre + priceFlower + " €"}</p>
     </div>
 }
 
